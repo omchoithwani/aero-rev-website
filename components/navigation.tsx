2077e20647cc57889f4389cca9cbc4f8b1ee4793
omchoithwani <omchoithwani@gmail.com>
2026-04-29 16:11:18 +0530

    Increase logo size to 80px height

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -49,7 +49,7 @@ export function Navigation() {
               alt="AeroRev"
               width={180}
               height={50}
-              className="w-auto h-auto max-h-24"
+              className="w-auto h-auto max-h-28"
               priority
             />
           </Link>
